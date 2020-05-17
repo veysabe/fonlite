773461c0a5f69e7d6c15cd8e6ac84c7a3018a165
Pavel Shein <veysabe@gmail.com>
2020-05-17 20:09:36 +0300

    almost done

---
+++ js/__content-block.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  $('.content__item-phone-slider').slick({
+    infinite: true,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 4000,
+    prevArrow: '<button type="button" class="slick-prev"><div class="slider-left-arrow"></div></button>',
+    nextArrow: '<button type="button" class="slick-next"><div class="slider-right-arrow"></div></button>',
+  });
+  $(".content__item-phone-slider").slick('refresh');
+  $('.number__input').mask("+7(999)999-99-99");
+});
+
+$("#feedbackform__submit").on('click', function() {
+    let name = $('.name__input').val();
+    let number = $('.number__input').val();
+    if (number != "" && name != "") {
+        alert('ok');
+        // $.ajax(
+        //     "/feedback.php",
+        //     {
+        //         name: name,
+        //         number: number,
+        //     },
+        //     onAjaxSuccess
+        // );
+        // function onAjaxSuccess(data)
+		// {
+		// 	console.log(data)
+        // }
+    } else {
+        alert('ne ok');
+    }
+});
--- js/__menu-block.js
+++ js/__menu-block.js
@@ -1,26 +1,25 @@
 $(function() {
-    $('.menu__choise').flexslider({
-      namespace: "slider-",
-      animation: "slide",
-      controlNav: false,
-      animationLoop: false,
-      directionNav: false,
-      slideshow: false,
-      selector: ".slides > .menu__items > li",
-      itemWidth: 100,
-      itemMargin: 20,
-      asNavFor: '#content'
-    });
-   
-    $('#content').flexslider({
-      namespace: "slider-",
-      animation: "slide",
-      controlNav: false,
-      directionNav: false,
-      animationLoop: false,
-      slideshow: false,
-      slideshowSpeed: 4000,
-      pauseOnHover: true,
-      sync: ".menu__choise"
-    });
+  $('.menu__choise').flexslider({
+    namespace: "slider-",
+    animation: "slide",
+    controlNav: false,
+    animationLoop: true,
+    directionNav: false,
+    slideshow: false,
+    selector: ".slides > .menu__items > li",
+    itemWidth: 100,
+    itemMargin: 20,
+    asNavFor: '#content',
   });
+  
+  $('#content').flexslider({
+    namespace: "slider-",
+    animation: "slide",
+    controlNav: false,
+    directionNav: false,
+    animationLoop: false,
+    slideshow: false,
+    pauseOnHover: true,
+    sync: ".menu__choise"
+  });
+});
